7f00010cfdc9baf27e5e472005f08b09c77d9ee2
Erik Åsén <93997033+agreedSkiing@users.noreply.github.com>
2024-09-06 13:24:47 +0200

    test: hibernate orm with same query

--- src/main/java/MemoryCache.java
+++ src/main/java/MemoryCache.java
@@ -4,49 +4,39 @@ import static java.lang.String.join;
 import io.quarkus.logging.Log;
 import io.quarkus.runtime.Startup;
 import io.quarkus.scheduler.Scheduled;
-import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityManager;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
-import org.hibernate.reactive.mutiny.Mutiny;
 
 @ApplicationScoped
 public class MemoryCache {
 
-  private final Mutiny.SessionFactory sf;
+  private final EntityManager em;
 
   private final Map<UUID, Code> codeCache = new ConcurrentHashMap<>();
 
-  public MemoryCache(Mutiny.SessionFactory sf) {
-    this.sf = sf;
+  public MemoryCache(EntityManager em) {
+    this.em = em;
   }
 
   @Startup
   public void buildCache() {
-    sf
-      .withSession(session ->
-        session
-          .createQuery("from Code c", Code.class)
-          .getResultList()
-          .chain(list -> {
-            list.forEach(code ->
-              codeCache.put(code.getResourceIdentifier(), code)
-            );
-            return Uni.createFrom().item(list);
-          })
-      )
-      .onFailure()
-      .transform(failure ->
-        new IllegalStateException("Failed to complete build of cache", failure)
-      )
-      .invoke(() -> Log.infof("Built cache with %d codes", codeCache.size()))
-      .subscribe()
-      .with(item -> Log.debugf("Done with startat event %s", item));
+    try {
+      em
+        .createQuery("from Code c", Code.class)
+        .getResultStream()
+        .forEach(code -> codeCache.put(code.getResourceIdentifier(), code));
+    } catch (Exception e) {
+      throw new IllegalStateException("Failed to complete build of cache", e);
+    }
+    Log.infof("Built cache with %d codes", codeCache.size());
+    Log.debug("Done with startat event");
   }
 
   @Scheduled(every = "5s", delayed = "5s", concurrentExecution = SKIP)
-  public Uni<Void> updateCache() {
+  public void updateCache() {
     var codesUUID = codeCache.keySet().stream().toList();
     var findRemovedCodes = new StringBuilder("(values"); // https://stackoverflow.com/questions/17209460/select-those-not-found-in-in-list
     // var findRemovedCodes = new StringBuilder("(select tmp.resource_id from (values("); // https://stackoverflow.com/questions/17209460/select-those-not-found-in-in-list
@@ -61,13 +51,10 @@ public class MemoryCache {
     var findAddedCodes =
       "union select x.resource_id from code x where x.resource_id not in :resource_id";
     var query = join(" ", findRemovedCodes.toString(), findAddedCodes);
-    return sf
-      .withSession(session ->
-        session
-          .createNativeQuery(query, UUID.class)
-          .setParameter("resource_id", codesUUID)
-          .getResultList()
-      )
-      .replaceWithVoid();
+
+    em
+      .createNativeQuery(query, UUID.class)
+      .setParameter("resource_id", codesUUID)
+      .getResultList();
   }
 }
